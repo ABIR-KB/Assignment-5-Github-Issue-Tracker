7a8824981a71e6a32bfae4453601add258ad6c9f
ABIR-KB <abirkumarbhowmik123@gmail.com>
2026-03-09 23:28:09 +0600

    error handeling for github live links

--- js/login.js
+++ js/login.js
@@ -4,7 +4,10 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
   const password = document.getElementById("password").value;
   if (username === "admin" && password === "admin123") {
     sessionStorage.setItem("github-issues-auth", "true");
-    window.location.href = "../dashboard/dashboard.html";
+    
+  
+    window.location.href = "./dashboard/dashboard.html";
+    
   } else {
     alert("Invalid username or password.");
   }
